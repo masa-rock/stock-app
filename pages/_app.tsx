001d738926e9a861a42a8ae65b0408a329b58cdb
masa-rock <79382412+masa-rock@users.noreply.github.com>
2023-05-07 23:48:39 +0900

    amplifyと連携

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Amplify } from "aws-amplify"
+import { withAuthenticator } from "@aws-amplify/ui-react"
+import "@aws-amplify/ui-react/style.css"
+
+import awsExports from "../src/aws-exports"
+Amplify.configure(awsExports)
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
